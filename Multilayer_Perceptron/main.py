8f88c70a9ec5c6408aad7d38209987a995724f3f
Aamir Khan Maarofi <kaamir600@gmail.com>
2020-10-17 15:44:38 +0500

    Modified main.py, added gen_data() method to get sample data, main() method to make call hirarchy of the program flow. Will update this once I'm done with Dense and MultiLayerPerceptron classes in dense.py and mlp.py respectively

--- Multilayer_Perceptron/main.py
+++ Multilayer_Perceptron/main.py
@@ -1,1 +1,33 @@
-#
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+from mlp import MultiLayerPerceptron
+
+def main():
+    print("In Main .. Starting ..")
+    
+    #Generating Data with 200 samples  containing 5 features and two traget classes
+    data = make_classification(n_samples=200, n_features=5, n_classes=2)
+        
+    #Spliting data into test and train sets
+    X_train, X_test, y_train, y_test = train_test_split(data)
+
+    #Normalizing the data in range [0,1]
+    X_train = X_train/X_train.max()
+    X_test = X_test/X_test.max()
+
+    #Creating nework 
+    MLP = MultiLayerPerceptron()
+
+    #Training network
+    #MLP.train()
+
+    #Testing network
+    #MLP.test()
+
+    #Validating network
+    #MLP.validate()
+
+    #New Predictions
+    #MLP.predict()
+if __name__ == "__main__":
+    main()
