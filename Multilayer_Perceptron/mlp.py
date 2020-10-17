8063480775641a2b3662b3efda3435face6258f2
Aamir Khan Maarofi <kaamir600@gmail.com>
2020-10-17 15:40:25 +0500

    Modified mlp.py, add new class MultiLayerPerceptron, will work on this once I finish the Dense class implementation in dense.py

--- Multilayer_Perceptron/mlp.py
+++ Multilayer_Perceptron/mlp.py
@@ -1,1 +1,29 @@
-#
+class MultiLayerPerceptron():
+    """
+    MultiLayerPerceptron represent fully connected multilayer perceptron network
+
+    ...
+
+    Attributes
+    ----------
+
+    Methods
+    -------
+
+    """
+
+    def __init__(self, ):
+            
+        pass
+    
+    def train(self):
+        pass
+
+    def test(self):
+        pass
+
+    def validate(self):
+        pass
+
+    def predict(self):
+        pass
