620db2d66ce4cdcf6d3b12d98ff4f7cdec5135c0
Aamir Khan Maarofi <kaamir600@gmail.com>
2020-10-19 00:21:05 +0500

    Implemented Forward Pass in dense.py and have added experimental code to main.py that I will be updating while implementing BackwardPass"

--- Multilayer_Perceptron/dense.py
+++ Multilayer_Perceptron/dense.py
@@ -9,27 +9,27 @@ class Dense():
 
     Attributes
     ----------
-    weights : 2D ndarray, shape = (input_dim, num_nodes)
+    weights : 2D ndarray, shape = (n_inputs, n_nodes)
         Stores the weights of all neurons in the layer
-    outputs : 2D ndarray of 'ones', shape = (num_nodes, 1)
+    outputs : 2D ndarray of 'ones', shape = (n_nodes, 1)
         Stores the outputs of all neurons in the layer
-    biases : 2D ndarray of 'ones', shape = (num_nodes, 1)
+    biases : 2D ndarray of 'ones', shape = (n_nodes, 1)
         Stores the biases of all neurons in the layer
-    activation_potentials : 2D ndarray 'zeros', shape = (num_nodes, 1)
+    activation_potentials : 2D ndarray 'zeros', shape = (n_nodes, 1)
         Store the  activation_potentials of all the neurons in the layer
-    local_grads : 2D ndarray 'zeros', shape = (num_nodes, 1)
+    local_grads : 2D ndarray 'zeros', shape = (n_nodes, 1)
         Store local gradients of all neurons in the layer
     
     Methods 
     -------
-    Constructor(input_dim, num_nodes) :
+    Constructor(n_inputs, n_nodes) :
         Initialize all the attributes mentioned above, as  
     act_poten() : 
         Calculate activation potential of the current layer neurons
-    activate(inputs) : 
+    sigmoid(inputs) : 
         Calculate activation potential of the current layer neurons
         Using formula inputs * weights + biases
-    d_activate() :
+    d_sigmoid() :
         Activates the activation_potential -> ndarray (act_potentials of all neurons in layer)
         Save the activated ndarry to outputs -> ndarray (outputs of all neurons in this layer)
     loc_grad(acc_error, layer, network) : 
@@ -41,60 +41,69 @@ class Dense():
     backward_pass() : 
 
     """
-    def __init__(self, input_dim, num_nodes):
-        #Weights associated with all num_nodes for input_dim synaptic inputs
-        self.weights = np.random.uniform(low=0, high=1, size=(input_dim, num_nodes))
+    def __init__(self, n_inputs, n_nodes):
+        #Weights associated with all n_nodes for n_inputs synaptic inputs
+        #Each column in the weight matrix is weight vector associated with one neuron
+        self.weights = np.random.uniform(low=0, high=1, size=n_inputs*n_nodes).reshape(n_inputs, n_nodes)
 
-        #Outputs of all the neurons in the layer, shape (num_nodes, 1)
-        self.outputs = np.ones((num_nodes, 1))
-
-        #Biases associated to each neuron, assuming the synaptic connection associated to biases is '1'
-        self.biases = np.ones((num_nodes, 1))
-        
-        #activation_potentials (Linear Combination) of all neurons in the layer
-        self.activation_potentials = np.zeros((num_nodes, 1))
-        
-        #Local Gradients associated with each node of the current layer
-        self.loacal_grads = np.zeros((num_nodes, 1))
-    
+        #Biases associated to each neuron, shape (1, n_nodes)
+        #There are n_nodes cloumns and each one represent bias associated with one neuron in layer
+        #This one dimension array will be added to linear cobinations of all neurons  
+        #assuming the synaptic connection associated to biases is '1'
+        self.biases = np.ones(n_nodes)
+                
     def act_poten(self, inputs):
         #Calculate activation potential of the current layer neurons -> ndarray 
-        #Formula (Input * Weight) + bias 
-        self.activation_potentials = np.dot(self.weights, inputs.T) + self.biases
+        #Which is inputs times weights add a bias
+        #Stores it to activation_potentials
+        self.activation_potentials = np.dot(inputs.T, self.weights) + self.biases
     
-    def activate(self):
-        #Activates the activation_potential -> ndarray (act_potentials of all neurons in layer)
-        #Save the activated ndarry to outputs -> ndarray (outputs of all neurons in this layer)
-        self.outputs = 1 / (1 - np.exp(self.activation_potentials))
+    def sigmoid(self):
+        #Activates the activation_potential -> ndarray
+        #Save the activated ndarry to outputs
+        self.outputs = 1 / (1 + np.exp(-self.activation_potentials))
 
-    def d_activate(self):
+    def d_sigmoid(self):
         #Derivitive of the activation potential -> ndarray (For all neurons, values of first differentail activation function at activation_potential)
         #Will be used in local gradient calculation
-        return self.activation_potentials * (1 - self.activation_potentials)
+        return np.dot(self.outputs, (1 - self.outputs))
 
-    def loc_grad(self, acc_error, layer, network):
+    def loc_grad(self, targets, prev_loc_grads, prev_weights, layer, network):
         #Calculation of local gradients of all neurons in this layer -> ndarray
         #Save this to local gradietns -> ndarray
         if layer == network[-1]:
-            # Output layer = error * derivative of activation function
-            
-            pass
+            #Output layer = error_at_end * derivative of activation function
+            error_at_end = (1/2) * np.power((layer.outputs - targets), 2)
+            self.loc_gradients = np.dot(error_at_end, self.d_sigmoid())
         else:
-            # Hidden layers = derivative of activation function * sum of all (derivative of activation
-            # function of next layer * weights associated with this neuron going to all neurons in next layer)
-            pass
+            # Hidden layers => derivative of activation function * sum of all (derivative of activation
+            # function of previous layer * (loc_grad vector of previous layer * weights vector of previous layer
+            # associated with output of current layer, which is input to previous layer))
+            self.loc_gradients = np.dot(self.d_sigmoid(), np.dot(prev_loc_grads, prev_weights))
 
     def updater(self):
         #Update Weights and Biases, based on learning rate, local gradients and error
         
         pass
 
-    def forward_pass(self):
+    def forward_pass(self, inputs):
         #inputs times weights add a bias and activate
         #calculate error,  this doesn't fit here, but will find it out
-        pass
+        self.act_poten(inputs)
+        self.sigmoid()
 
-    def backward_pass(self):
+    def backward_pass(self, targets, prev_loc_grads, prev_weights, layer, network):
         #Calculate local gradient of all the neurons in this layer
         #Update weights and biases
-        pass
+        self.loc_grad(targets, prev_loc_grads, prev_weights, layer, network)
+        print('Got (targets)        : ', targets)
+        print('Shape                : ', targets.shape)
+        print('Got (prev_loc_grad)  : ', prev_loc_grads)
+        print('Shape                : ', prev_loc_grads.shape)
+        print('Got (prev_weights)   : ', prev_weights)
+        print('Shape                : ', prev_weights.shape)
+        print('Got (layer)          : ', layer)
+        print('Got (network)        : ', network)
+        print('Local Gradients: ', self.loc_gradients)
+        print('Shape                : ', self.loc_gradients.shape)
+
--- Multilayer_Perceptron/main.py
+++ Multilayer_Perceptron/main.py
@@ -1,20 +1,65 @@
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 from mlp import MultiLayerPerceptron
+from dense import Dense
 
 def main():
     print("In Main .. Starting ..")
     
     #Generating Data with 200 samples  containing 5 features and two traget classes
     data = make_classification(n_samples=200, n_features=5, n_classes=2)
-        
-    #Spliting data into test and train sets
-    X_train, X_test, y_train, y_test = train_test_split(data)
 
+    #Spliting data into test and train sets
+    X_train, X_test, y_train, y_test = train_test_split(data[0], data[1])
+    
     #Normalizing the data in range [0,1]
     X_train = X_train/X_train.max()
     X_test = X_test/X_test.max()
 
+    #-------------------------------Experimental Code-----------------------------------------
+    
+    print('-------------------------------------------------------------------')
+    print("Layer: Dense1: ")
+    print("-----------------------Forward Pass Layer 1------------------------")
+
+    dense1  = Dense(5, 8)
+    print('Inputs Shape  : ', X_train[0].shape)
+    print('Weights Shape : ', dense1.weights.shape)
+    dense1.forward_pass(X_train[0])
+
+    print("Max in act_pot: ", dense1.activation_potentials.max())
+    print("Min in act_pot: ", dense1.activation_potentials.min())
+    print()
+    print("Max in outputs: ", dense1.outputs.max())
+    print("Min in outputs: ", dense1.outputs.min())
+    print("-----------------------Forward Pass Layer 1------------------------")
+    print('-------------------------------------------------------------------')
+    print("Layer: Dense2: ")
+    print("-----------------------Forward Pass Layer 2------------------------")
+
+    dense2 = Dense(8, 2)
+    print("Input Shape   : ", dense1.outputs.shape)
+    print("Weights Shape : ", dense2.weights.shape)
+    dense2.forward_pass(dense1.outputs)
+
+    print("Max in act_pot: ", dense2.activation_potentials.max())
+    print("Min in act_pot: ", dense2.activation_potentials.min())
+    print()
+    print("Max in outputs: ", dense2.outputs.max())
+    print("Min in outputs: ", dense2.outputs.min())
+    print("-----------------------Forward Pass Layer 2-------------------------")
+    print('--------------------------------------------------------------------')
+    print('Layer: Dense2: ')
+    print("-----------------------Backward Pass Layer 2------------------------")
+    print("-----------------------Backward Pass Layer 2------------------------")
+    print('--------------------------------------------------------------------')
+    print("Layer: Dense1: ")
+    print("-----------------------Backward Pass Layer 1------------------------")
+    print("-----------------------Backward Pass Layer 1------------------------")
+
+
+    #-------------------------------Experimental Code-----------------------------------------
+    '''
     #Creating nework 
     MLP = MultiLayerPerceptron()
 
@@ -29,5 +74,7 @@ def main():
 
     #New Predictions
     #MLP.predict()
+
+    '''
 if __name__ == "__main__":
     main()
