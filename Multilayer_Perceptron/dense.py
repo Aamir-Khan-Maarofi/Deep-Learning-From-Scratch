Fixed the gradient direction to downhill travel

Multilayer_Perceptron/dense.py
@@ -4,12 +4,12 @@ of a layer in fully connected multilayer perceptron
 '''
 
 
-#Importing numpy for vector operations
+# Importing numpy for vector operations
 import numpy as np
 
-#Setting random seed to generate similar weights an biases vector in each execution
-#I set the seed for random, just to inspect the current and updated weights
-np.random.seed(78)
+# Setting random seed to generate similar weights an biases vector in each execution
+# I set the seed for random, just to inspect the current and updated weights
+np.random.seed(9)
 
 
 class Dense():
@@ -29,7 +29,7 @@ class Dense():
         Store the  activation_potentials of all the neurons in the layer
     local_grads : 1D vector, shape = (n_nodes,)
         Store local gradients of all neurons in the layer
-    
+
     Methods 
     -------
     Constructor(n_inputs, n_nodes) :
@@ -55,16 +55,17 @@ class Dense():
         n_inputs    : Number of inputs to this layer
         n_nodes     : Number of nodes in this layer
         '''
-        
-        #Weights associated with all n_nodes for (n_inputs) synaptic inputs, shape = (n_inputs, n_nodes)
-        #Each column in the weight matrix is weight vector associated with one neuron
-        self.weights = np.random.uniform(low=0, high=1, size=n_inputs*n_nodes).reshape(n_inputs, n_nodes)
-        
-        #Biases associated to each neuron, shape (n_nodes,)
-        #There are n_nodes elements and each one represent bias associated with one neuron in layer
-        #This one dimension array will be added to linear cobinations of all neurons  
-        #assuming the synaptic connection associated to biases is conceptually '1'
-        self.biases = np.ones(n_nodes)
+
+        # Weights associated with all n_nodes for (n_inputs) synaptic inputs, shape = (n_inputs, n_nodes)
+        # Each column in the weight matrix is weight vector associated with one neuron
+        self.weights = np.random.uniform(
+            low=-0.1, high=0.1, size=n_inputs*n_nodes).reshape(n_inputs, n_nodes)
+
+        # Biases associated to each neuron, shape (n_nodes,)
+        # There are n_nodes elements and each one represent bias associated with one neuron in layer
+        # This one dimension array will be added to linear cobinations of all neurons
+        # assuming the synaptic connection associated to biases is conceptually '1'
+        self.biases = np.random.uniform(low=0.1, high=0.2, size=n_nodes)
 
     def sigmoid(self):
         '''
@@ -74,10 +75,10 @@ class Dense():
         Operation : Uses sigmoid function which operates on all activation potentials of the layer
         '''
 
-        #Activates the activation_potential -> ndarray
-        #Save the activated ndarry to outputs
+        # Activates the activation_potential -> ndarray
+        # Save the activated ndarry to outputs
         self.outputs = 1 / (1 + np.exp(-self.activation_potentials))
-  
+
         return self.outputs
 
     def d_sigmoid(self):
@@ -89,9 +90,9 @@ class Dense():
                     differential of activation function, which will be used in local gradient calculation
         '''
 
-        # Vector value of shape (n_nodes,) 
+        # Vector value of shape (n_nodes,)
         return self.outputs * (1 - self.outputs)
-        
+
     def forward_pass(self, inputs):
         '''
         forward_pass : Forward pass of current layer for inputs
@@ -99,15 +100,16 @@ class Dense():
         returns      : output vector of layer 
         operations   : Find activation potentials, call activation function and return outputs   
         '''
-        
-        #Calculate activation potential of the current layer neurons -> ndarray 
-        #Which is inputs times weights add a bias and stores it to activation_potentials
-        self.activation_potentials = np.dot(self.weights.T, inputs) + self.biases
-        
+
+        # Calculate activation potential of the current layer neurons -> ndarray
+        # Which is inputs times weights add a bias and stores it to activation_potentials
+        self.activation_potentials = np.dot(
+            self.weights.T, inputs) + self.biases
+
         return self.sigmoid()
 
     def backward_pass(self, learning_rate, inputs_to_layer, target,
-                      prev_loc_grads = [], prev_weights = []):
+                      prev_loc_grads=[], prev_weights=[]):
         '''
         backward_pass : Backward pass of current layer
         Note          : Backward signal origninates at output node, propegate all the way to 
@@ -118,15 +120,15 @@ class Dense():
                         to empty lists. This specifies the case of call to backward pass with output
                         layer,  hence there is no layer preceeding output layer, so no local gradients
                         and weights will be passed when call to this method is made with output layer,               
-        
+
         Params        : learning_rate   -> Step size of gradient descent
                         inputs_to_layer -> Synaptic inputs of the layer
                         target          -> One hot encoded output corresponding to training sample
                                            used in forward pass, need this for error computation 
                         prev_loc_grads  -> Local gradient of layer next to current layer
                         prev_weights    -> Synaptic weights of layer next to current layer
-        
-        Returns       : Updated weights and biases of current layer, that will be used in total error computation
+
+        Returns       : None
 
         Operations    : Confirms if previous local gradients are not passed then the call is made 
                         with output layer, in this case, computes relative error of output neurons
@@ -137,42 +139,40 @@ class Dense():
                         Once local gradients are calculated, this method updates weights and biases, and 
                         return the updated weights and biases to caller.  
         '''
-        
-        #If prev_loc_grads length is '0' then if is executed, which indicates that no local gradients
-        #are passed, this is the case when call is made  with output layer as no layer preceeds output layer
+
+        # If prev_loc_grads length is '0' then if is executed, which indicates that no local gradients
+        # are passed, this is the case when call is made  with output layer as no layer preceeds output layer
         if not len(prev_loc_grads):
-            
-            #For output layer, firstly relative error is calculated,
-            #error_at_end_nodes is vector of shape (n_nodes,), same is target, their element-wise
-            #subtraction results in vector of same dimensions and size. 
-            self.error_at_end_nodes = self.outputs - target
-            
-            #Secondly, uses the computed error and d_sigmoid() to calculate local gradients
-            #Based on formula: local gradients at output = e_k * derivitive of sigmoid
-            self.loc_gradients = self.error_at_end_nodes * self.d_sigmoid()
+
+            # For output layer, firstly relative error is calculated,
+            # error_at_end_nodes is vector of shape (n_nodes,), same is target, their element-wise
+            # subtraction results in vector of same dimensions and size.
+            self.error_at_end_nodes = target - self.outputs
+
+            # Secondly, uses the computed error and d_sigmoid() to calculate local gradients
+            # Based on formula: local gradients at output = e_k * derivitive of sigmoid
+            self.loc_gradients = (self.error_at_end_nodes * self.d_sigmoid())
         else:
             # again, previous here refer to layer prceeding current layer (layer next to current)
-            # Hidden layers local gradients => derivative of activation function * 
+            # Hidden layers local gradients => derivative of activation function *
             # sum of all (local gradients * weights) of neurons associated with output of current layer, which is input to previous layer))
-            
-            #Initialize temp with containing all zeros
+
+            # Initialize temp with containing all zeros
             temp = np.zeros(prev_weights.shape[0])
 
-            #The loop iterates number of neurons in previous layer, as each neuron has one loc_grad
+            # The loop iterates number of neurons in previous layer, as each neuron has one loc_grad
             for i in range(prev_loc_grads.size):
-                #Compute local gradient times weights of the previous layer and add it to total
-                #The multiplied weights is a vector
+                # Compute local gradient times weights of the previous layer and add it to total
+                # The multiplied weights is a vector
                 temp += prev_loc_grads[i] * prev_weights[:, i]
-            
-            #Compute the  local gradient of hidden layer based on formula
-            #local gradient of hidden layer = derivitive of sigmoid * sum of all (local grads * weights)
-            self.loc_gradients = self.d_sigmoid() * temp
-            
-        #Update Weights and Biases, based on learning rate, local gradients and inputs to layer
-        self.weights = self.weights + (learning_rate * np.outer(inputs_to_layer, self.loc_gradients))
-        
-        #The inputs_to_layer is ommited as bias is (conceptually) caused by input from a neuron with a fixed activation of 1
-        self.biases = self.biases + learning_rate * self.loc_gradients
 
-        #return updated weights and biases
-        return self.weights, self.biases
+            # Compute the  local gradient of hidden layer based on formula
+            # local gradient of hidden layer = derivitive of sigmoid * sum of all (local grads * weights)
+            self.loc_gradients = (self.d_sigmoid() * temp)
+
+        # Update Weights and Biases, based on learning rate, local gradients and inputs to layer
+        self.weights = self.weights + \
+            (learning_rate * np.outer(inputs_to_layer, self.loc_gradients))
+
+        # The inputs_to_layer is ommited as bias is (conceptually) caused by input from a neuron with a fixed activation of 1
+        self.biases = self.biases + learning_rate * self.loc_gradients

Multilayer_Perceptron/main.py
@@ -1,137 +1,146 @@
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder, normalize
 
 import matplotlib.pyplot as plt
 from dense import Dense
 import numpy as np
 import random
 
+
 def main():
     print("In Main .. Starting ..")
-    
-    #Generating Data with 10 samples containing 4 features, one target calss 
-    #The target in binary either 0 or 1
-    data = make_classification(n_samples=10, n_features=4, n_classes=2)
 
-    #Spliting training features and training targets
+    # Generating Data with 10 samples containing 4 features, one target calss
+    # The target in binary either 0 or 1
+    data = make_classification(
+        n_samples=100, n_features=4, n_informative=4, n_redundant=0, n_classes=2)
+
+    # Spliting training features and training targets
     X_train = data[0]
     y_train = data[1]
 
-    #Normalizing the data in range [0,1]
-    X_train = X_train/X_train.max()
+    # Normalizing the data in range [0,1]
+    X_train = normalize(X_train)
+
+    plt.plot(X_train)
+    plt.show()
 
     print('Training Data: (X)   : ', X_train)
     print('Taraining Data: (y)  : ', y_train)
-    
-    #Encoding data using one hot encoding technique 
-    enc = OneHotEncoder(sparse = False)
-    desired = enc.fit_transform(y_train.reshape(-1,1))
-    print("One Hot encoded: (y) : ", desired)
-    
 
-    #-------------------------------Experimental Code-----------------------------------------
-    #netwok is a list of all layers in the network, each time a layer is created as Dense() object, will be appended to network
+    # Encoding data using one hot encoding technique
+    enc = OneHotEncoder(sparse=False)
+    desired = enc.fit_transform(y_train.reshape(-1, 1))
+    print("One Hot encoded: (y) : ", desired)
+
+    # -------------------------------Experimental Code-----------------------------------------
+    # netwok is a list of all layers in the network, each time a layer is created as Dense() object, will be appended to network
     network = []
     print("----------------------------Layer 1-------------------------")
-    network.append(Dense(4,6))
-    print('Created Only Hidden Layer: N_nodes: {} , N_inputs: {}'.format(6,4))
+    network.append(Dense(4, 6))
+    print('Created Only Hidden Layer: N_nodes: {} , N_inputs: {}'.format(6, 4))
+
     print("----------------------------Layer 2-------------------------")
-    network.append(Dense(6,2)) 
-    print('Created Output Layer: N_nodes: {} , N_inputs: {}'.format(2,6))    
-    
-    
-    
-    #List to store the total error
+    network.append(Dense(6, 2))
+    print('Created Output Layer: N_nodes: {} , N_inputs: {}'.format(2, 6))
+
+    # List to store the total error
     error = []
-    
-    #initialized epoch to 1
+
+    # initialized epoch to 1
     epoch = 1
-    
-    #The main training loop, exits at after 10 epochs
-    while epoch <= 10:
-        
+    # The main training loop, exits at after 10 epochs
+    while epoch <= 100:
+
         # Random suffling of training data
-        temp = list(zip(X_train,y_train))
+        temp = list(zip(X_train, y_train))
         random.shuffle(temp)
-        X_train,y_train=zip(*temp)
-
-        #Variabels to store  the temporary state of the operations
-        weights = 0
-        biases = 0
-
-        #List to store the intermediate weights and biases for means square error calculation at end of each epoch
-        wb = []
+        X_train, y_train = zip(*temp)
 
         print('--------------------------Epoch: {}-------------------------'.format(epoch), '\n')
 
-        #Select one feature vector from feature matrix and corresponding target vector from desired matrix (Which was obtained from one hot encoding)
-        for x,y in zip(X_train, desired):     
-            
-            #previous_inputs list keeps track of inputs from layer to layer in each epoch
+        # Select one feature vector from feature matrix and corresponding target vector from desired matrix (Which was obtained from one hot encoding)
+        for x, y in zip(X_train, desired):
+
+            # previous_inputs list keeps track of inputs from layer to layer in each epoch
             previous_inputs = []
-            
-            #At start of each epoch, the list contains only inputs from input nodes which are the features of current training sample
+
+            # At start of each epoch, the list contains only inputs from input nodes which are the features of current training sample
             previous_inputs.append(x)
 
-            #This loop iterates over all layers in network and perform forward pass
+            # This loop iterates over all layers in network and perform forward pass
             for layer in network:
-                #Forward_pass perform forward propagation of a layer of last element of the previous_inputs list, 
-                #and returns the output of layer which is stored as ndarray in list, as it will be used as inputs to next layer    
+                # Forward_pass perform forward propagation of a layer of last element of the previous_inputs list,
+                # and returns the output of layer which is stored as ndarray in list, as it will be used as inputs to next layer
                 previous_inputs.append(layer.forward_pass(previous_inputs[-1]))
-            
-            #Ignore the output of last layer, as I'm using the preious_inputs array in reverse order in backward_pass in next loop
+
+            # Ignore the output of last layer, as I'm using the preious_inputs array in reverse order in backward_pass in next loop
             previous_inputs = previous_inputs[:-1]
 
-            #Next loop reverses the network and previous_inputs lists to perform  backward propagation of al layers from output layer all the way to input layer
+            # Next loop reverses the network and previous_inputs lists to perform  backward propagation of al layers from output layer all the way to input layer
             for layer, prev_inputs in zip(network[::-1], previous_inputs[::-1]):
-                
-                #If the layer is not output layer then perform backward propagation using code inside if statement
+
+                # If the layer is not output layer then perform backward propagation using code inside if statement
                 if layer != network[-1]:
 
-                    #call to backward_pass using learning rate = 0.0001, inputs to current layer, target vector 'y', 
-                    #previous_loc_gradients (local gradients of layer next to current layer),
-                    #and prev_weights (weights of layer next to current layer) 
-                    #Store the updated weights and biases for mean square error calculation at end of epoch
-                    weights, biases = layer.backward_pass(0.0001, prev_inputs, y, prev_loc_gradients, prev_weights)
-                
-                #otherwise, perform the backward pass for output layer using code in else block
+                    # call to backward_pass using learning rate = 0.0001, inputs to current layer, target vector 'y',
+                    # previous_loc_gradients (local gradients of layer next to current layer),
+                    # and prev_weights (weights of layer next to current layer)
+                    # Store the updated weights and biases for mean square error calculation at end of epoch
+                    layer.backward_pass(
+                        0.1, prev_inputs, y, prev_loc_gradients, prev_weights)
+
+                # otherwise, perform the backward pass for output layer using code in else block
                 else:
-                    weights, biases = layer.backward_pass(0.0001, prev_inputs, y)
-                
-                #Store local gradietns nad weights of current layer for next layer backward pass
+                    layer.backward_pass(
+                        0.1, prev_inputs, y)
+
+                # Store local gradients and weights of current layer for next layer backward pass
                 prev_loc_gradients = layer.loc_gradients
                 prev_weights = layer.weights
-                
-                #Add updated weights and biases to wb, will be using it in next loop
-                wb.append((weights, biases))
 
-            #error_i is sum of errors for all training examples on updated weights and biases
+            # error_i is sum of errors for all training examples on updated weights and biases
             error_i = 0
 
-            #This loop calculates Total Error on new weights and biases, by considering the whole training data
+            # This loop calculates Total Error on new weights and biases, by considering the whole training data
             for x_val, y_val in zip(X_train, desired):
-                previous_inputs = []    
+                previous_inputs = []
                 previous_inputs.append(x_val)
 
-                #Perform  forward pass on new weights and biases
+                # Perform  forward pass on new weights and biases
                 for layer in network:
-                    #Forward Pass
-                    previous_inputs.append(layer.forward_pass(previous_inputs[-1]))
-                
-                #add the error of prediction of current training sample to prevoius errors
-                error_i += np.power((previous_inputs[-1] - y_val), 2).sum()
-            
-            #Append total error of current sample to error list, and repeate the process for next sample, do this for all samples
-            error.append(error_i)
+                    # Forward Pass
+                    previous_inputs.append(
+                        layer.forward_pass(previous_inputs[-1]))
 
-        #Increase epoch by one, and perform forward, backward on next sample, and calculate error for all samples, do this until while  is true
+                # add the error of prediction of current training sample to pervious errors
+                error_i += np.power((previous_inputs[-1] - y_val), 2).sum()
+
+        # Append total error of current sample to error list, and repeat the process for next sample, do this for all samples
+        error.append(error_i)
+        print("-----------------")
+        print("Error: ", error_i)
+        print("-----------------")
+        print()
+
+        if error[epoch - 2] > error_i:
+            print('Decreasing...')
+        elif error[epoch - 2] == error_i:
+            print('Nothing is happening...')
+        else:
+            print('Increasing...')
+        # Increase epoch by one, and perform forward, backward on next sample, and calculate error for all samples, do this until while  is true
         epoch += 1
-    
-    #Plot the errors after training completes, 
-    plt.plot(error)
+
+    # Plot the errors after training completes,
+    plt.plot(error, color='r')
+    plt.xlabel('Epochs')
+    plt.ylabel('Error')
+    plt.title('Total Error Graph')
     plt.show()
-    #-------------------------------Experimental Code-----------------------------------------
+    # -------------------------------Experimental Code-----------------------------------------
+
 
 if __name__ == "__main__":
     main()